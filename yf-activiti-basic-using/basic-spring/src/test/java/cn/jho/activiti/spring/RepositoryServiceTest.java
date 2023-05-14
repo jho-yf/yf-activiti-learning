632abef37f83926abe82ce8633112a73f2b79252
jho-yf <xu-jihong@qq.com>
2023-05-14 17:53:52 +0800

    feat: 流程定义挂起与激活

--- yf-activiti-basic-using/basic-spring/src/test/java/cn/jho/activiti/spring/RepositoryServiceTest.java
+++ yf-activiti-basic-using/basic-spring/src/test/java/cn/jho/activiti/spring/RepositoryServiceTest.java
@@ -3,9 +3,12 @@ package cn.jho.activiti.spring;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.ZipInputStream;
+import org.activiti.engine.ActivitiException;
 import org.activiti.engine.impl.util.IoUtil;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -102,6 +105,49 @@ class RepositoryServiceTest extends AbstractTest {
         assertNotNull(deployment);
         LOGGER.info("deployment: {}", deployment);
         LOGGER.info("deployment id: {}", deployment.getId());
+    }
+
+    @Test
+    void testSuspendAndActivateProcessDefinition() {
+        String resource = "custom-bpmn-model.bpmn20.xml";
+        Deployment deployment = repositoryService.createDeployment()
+                .name("myprocess")
+                .category("mycategory")
+                .key("custom-bpmn-key")
+                .addClasspathResource(resource)
+                .deploy();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        final String processDefinitionId = processDefinition.getId();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        assertFalse(processInstance.isSuspended());
+        assertFalse(processDefinition.isSuspended());
+
+        repositoryService.suspendProcessDefinitionById(processDefinitionId, true, null);
+        processDefinition = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionId(processDefinitionId)
+                .singleResult();
+        processInstance = runtimeService.createProcessInstanceQuery()
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        assertTrue(processDefinition.isSuspended());
+        assertTrue(processInstance.isSuspended());
+
+        assertThrows(ActivitiException.class, () -> {
+            runtimeService.startProcessInstanceById(processDefinitionId);
+        });
+
+        repositoryService.activateProcessDefinitionById(processDefinitionId, true, null);
+        processDefinition = repositoryService.createProcessDefinitionQuery()
+                .processDefinitionId(processDefinitionId)
+                .singleResult();
+        processInstance = runtimeService.createProcessInstanceQuery()
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        assertFalse(processDefinition.isSuspended());
+        assertFalse(processInstance.isSuspended());
+        runtimeService.startProcessInstanceById(processDefinitionId);
     }
 
 }
